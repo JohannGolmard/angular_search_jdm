9027afcc77386b9ae8032a06c6dba616e8e8f157
golmard <johann.golmard@iut-valence.fr>
2019-10-09 17:05:41 +0200

    Réinstallation complète pour désinstaller angular Material, afin d'utiliser le framework Semantic UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    SearchResultComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
