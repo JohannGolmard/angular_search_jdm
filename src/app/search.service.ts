b86e3e3d96e7e0331fe727a6f1d2eab923d2f6ab
golmard <johann.golmard@gmail.com>
2020-01-12 23:04:34 +0100

    merge

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -8,6 +8,7 @@ import { Observable, Subject } from 'rxjs';
 })
 export class SearchService {
 
+  public server = "http://15.188.255.38:8888"
   public res: any;
   public _mot = new Subject<String>();
   public mot = this._mot.asObservable();
@@ -15,12 +16,12 @@ export class SearchService {
   constructor(private http: HttpClient) { }
 
   getDef(mot:any): Observable<any>{
-  	let observable: Observable<any> = this.http.get("http://localhost:8888/def/"+mot);
+  	let observable: Observable<any> = this.http.get(this.server+"/def/"+mot);
     return observable;
   }
 
   getRelation(mot:any, num:any): Observable<any>{
-    return this.http.get("http://localhost:8888/requete/"+mot+"/"+num);
+    return this.http.get(this.server+"/requete/"+mot+"/"+num);
   }
 
   public publish(data: String) {
