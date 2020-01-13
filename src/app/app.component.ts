74b8568dad7a0c96687792944fcec2fa547105f9
CedricDurand <cedricdurand13@sfr.fr>
2020-01-13 17:02:23 +0100

    Merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,11 @@ export class AppComponent {
   title = 'search-jdm';
 
   private lesRelations : any;
+  private allRelations : any;
 
   constructor() {
-    this.lesRelations = ["Domaine", "Objet" , "Caractéristique", "Synonyme", "Contraire"];
+    this.allRelations = ["Domaine"  ,"Partie Discours", "Synonyme", "Générique" , "Contraire", "Hyponyme", "Constituant", "Holonymes", "Locution", "Agent"  ,"Objet"  , "Lieu"  , "Instrument"   ,"Caractéristique"  , "Lemme", "Amplification", "Affaiblissement", "Famille", "Peut faire", "Peut faire (instrument)", "Inverse d'Objet", "Action lieu", "Sentiment", "Manières", "Signification","Rôle télique", "Role agentif", "Verbe action", "Action verbe", "Consequence", "Cause", "Adjectif verbe", "Verbe adjectif", "Temporelle", "Object matière", "Matière objet", "Suivre temporellement", "Produire", "Ressultat de", "Opposition", "Implication","Quantificateur", "Masculin", "Féminin" , "Strictement équivalent", "Agentif applicatif", "Instance de type", "Verbe réalise l'action", "Similaire", "Element compose l'ensemble", "Enesemble qui est composé par l'élément","Acteur processus", "Variante", "Termes strictement substituables", "Plus gros", "Moins gros", "Accompagné", "Patient du processus", "Partice passé", "Co-hyponymes", "Paricipe présent", "Instrument du processus", "Dérivation morphologique", "Auteur de l'oeuvre", "Personnage de l'oeuvre", "Nourriture animal", "Acteur film", "Mode de déplacement", "Interprète du personnage", "Couleur", "Cible de la maladie", "Symptomes maladie", "Précéder temporellement", "Diagnostique maladie", "Précéder spatialement", "Suivre spatialement", "Relation sociale", "Tributaire", "Sentiment associé", "Relié avec", "But de l'action", "Possede", "Qui est possede", "Auxiliaire verbe","Précéder logiquement", "Suivre logiquement", "Incompatible", "Requis", "Est instance de", "est concerné par", "Inverse de symptome", "Unité", "Favorise", "Circonstances", "Oeuvre de l'auteur", "Complément d'agent", "Beneficiaire", "Descende de", "Domaine de substitution", "Propriété", "Voix active verbe", "Ce qu'utilise un objet", "Est utilisé par", "Adjectif -> nom propriété", "Nom propriété -> adjectif", "Adjectif -> adverbe", "Adverbe -> adjectif", "Homophone", "Confusion potentielle", "Concerne", "Adjectif -> Nom", "Nom -> Adjectif", "Opinion", "Traduction", "TOTAKI", "Wikipédia", "Inhibition" ];
+    this.lesRelations = ["Domaine"  ,"Partie Discours", "Synonyme", "Générique" , "Contraire", "Hyponyme", "Constituant"];
   }
 
   upOrDown(val :string){
@@ -23,12 +25,12 @@ export class AppComponent {
   }
 
   up(val :string){
-    let indice = 0;
+    let indice = -1;
     for(let rela = 0 ; rela < this.lesRelations.length; rela++){
       if(this.lesRelations[rela]==val)
         indice = rela;
     }
-    if(indice != 0 ){
+    if(indice != 0 && indice != -1){
       let tempo = this.lesRelations[indice-1];
       this.lesRelations[indice-1]=val;
       this.lesRelations[indice]=tempo;
@@ -36,12 +38,13 @@ export class AppComponent {
   }
 
   down(val :string){
-    let indice = 0;
+    let indice = -1;
     for(let rela = 0 ; rela < this.lesRelations.length; rela++){
       if(this.lesRelations[rela]==val)
         indice = rela;
     }
-    if(indice != this.lesRelations.length-1 ){
+
+    if(indice != this.lesRelations.length-1 && indice != -1 ){
       let tempo = this.lesRelations[indice+1];
       this.lesRelations[indice+1]=val;
       this.lesRelations[indice]=tempo;
